a5baf775306bf3070b027ac028a0eaacc4eaa237
LeeeSHH <137856067+LeeeSHH@users.noreply.github.com>
2023-12-05 20:01:41 +0900

    Update firstWindow.py

--- firstWindow.py
+++ firstWindow.py
@@ -2,6 +2,14 @@ from PyQt5.QtGui import QIcon, QPixmap, QCursor
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
+import threading
+import time
+import json
+from socket import *
+import socket
+import sys
+
+from secondWindow import SecondWindow
 
 
 def styleButton(button):
@@ -22,8 +30,9 @@ def styleButton(button):
 
 class FirstWindow(QWidget):
     switch_window = QtCore.pyqtSignal()
-    def __init__(self):
+    def __init__(self, client_socket):
         super().__init__()
+        self.client_socket = client_socket
         self.setWindowTitle("Halligalli Game")
         self.setGeometry(0, 0, 900, 600)
 
@@ -50,11 +59,27 @@ class FirstWindow(QWidget):
             """
         )
         self.start_button.clicked.connect(self.secondWindowEmit)
+
+
         self.grid = QGridLayout()
         self.grid.addWidget(self.label, 1, 1)
         self.grid.addWidget(self.start_button, 2, 1)
         self.setLayout(self.grid)
         self.show()
 
+
     def secondWindowEmit(self):
+        self.client_socket.connect(('220.149.128.100', 4061))
+        self.thread = threading.Thread(target=self.run, daemon=True)
+        self.thread.start()
         self.switch_window.emit()
+
+    def run(self): # 플레이어 데이터 받아옴
+        while True:
+            self.recv_data = self.client_socket.recv(1024)
+            self.recv_data = json.loads(self.recv_data.decode())
+            time.sleep(0.1)
+            if(self.recv_data.get('id') != None):
+                player_id = self.recv_data.get('id')
+                player_card = self.recv_data.get('card')
+                print(player_id, player_card)
