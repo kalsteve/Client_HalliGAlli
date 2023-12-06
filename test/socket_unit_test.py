97676ec40b0ee2c8522c6a16288a26226ffb3052
kalsteve <kalsagansteve@gmail.com>
2023-12-06 16:59:43 +0900

    test

---
+++ test/socket_unit_test.py
@@ -0,0 +1,13 @@
+import socket from *
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def socket_test(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
